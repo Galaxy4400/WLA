135e4468a4d157e50555225a3b3c35521a265721
Moiseev Evgeny <moiseevEO@yandex.ru>
2023-09-15 01:51:51 +0300

    AppServiceProvider new settings

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,24 +2,34 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\DB;
+use Illuminate\Database\Connection;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     */
-    public function register(): void
-    {
-        //
-    }
+	/**
+	 * Register any application services.
+	 */
+	public function register(): void
+	{
+		//
+	}
 
-    /**
-     * Bootstrap any application services.
-     */
-    public function boot(): void
-    {
+	/**
+	 * Bootstrap any application services.
+	 */
+	public function boot(): void
+	{
 		Schema::defaultStringLength(191);
-    }
+
+		Model::preventLazyLoading(!app()->isProduction());
+		Model::preventSilentlyDiscardingAttributes(!app()->isProduction());
+
+		DB::whenQueryingForLongerThan(500, function (Connection $connection) {
+			// TODO
+		});
+	}
 }
